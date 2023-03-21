d5f6fac8657247268a72f2facc343000cd3fb553
megaseves <megaseves@gmail.com>
2023-03-21 17:13:41 +0100

    Add: Upload avatar drag & drop design

--- front-end-react/src/components/Photos/avatar/UploadAvatar.js
+++ front-end-react/src/components/Photos/avatar/UploadAvatar.js
@@ -1,28 +1,63 @@
 import React, {useState} from 'react';
 import axios from "axios";
+import './UploadAvatar.css';
 
 export function UploadAvatar(props) {
 
     const [file, setFile] = useState()
-    const [caption, setCaption] = useState("")
+    const [caption, setCaption] = useState("avatar")
 
     const submit = async event => {
-    event.preventDefault()
+        event.preventDefault()
 
-    const formData = new FormData();
-    formData.append("image", file)
-    formData.append("caption", caption)
-    formData.append("user_id", props.userDetail.user_id)
-    await axios.post("http://localhost:8080/uploadAvatar", formData, { headers: {'Content-Type': 'multipart/form-data'}})
+        const formData = new FormData();
+        formData.append("image", file)
+        formData.append("caption", caption)
+        formData.append("user_id", props.userDetail.user_id)
+        await axios.post("http://localhost:8080/uploadAvatar", formData, { headers: {'Content-Type': 'multipart/form-data'}})
+        window.location.assign("/profile");
+    }
+
+    const close = () => {
+        const uploadForm = document.querySelector('.upload-avatar-form');
+        const uploadContainer = document.querySelector('.upload-avatar-container');
+        if (uploadForm.classList.contains("open")) {
+            uploadForm.classList.remove("open");
+            uploadContainer.classList.remove("open");
+        }
     }
 
     return (
-        <div className={'form-container'}>
-            <form onSubmit={submit}>
-                <input onChange={e => setFile(e.target.files[0])} type="file" accept="image/*"></input>
-                <input value={caption} onChange={e => setCaption(e.target.value)} type="text" placeholder='Caption'></input>
-                <button type="submit">Submit</button>
-            </form>
-        </div>
+        <>
+            <div className={'upload-avatar-container'} onClick={() => close()} >
+            </div>
+            <div className="upload-avatar-form">
+                <form onSubmit={submit} onKeyDown={(e)=> e.key === 'Enter' && submit()}>
+                    <div className="upload-comp">
+                        <div className="upload-header">
+                            <h3>Update profile picture</h3>
+                        </div>
+
+{/*                        <label className={"upload-btn"} htmlFor="file">+ Upload photo</label>
+                        <input className={"upload-input"} name={"file"} id={"file"} onChange={e => setFile(e.target.files[0])} type="file" accept="image/*"></input>
+
+                        <input hidden value={caption} onChange={e => setCaption(e.target.value)} type="text" placeholder='Caption'></input>
+                        <button className={"submit-btn"} type="submit">Upload</button>*/}
+
+                         <div className={"new-drop-form"}>
+                            <label htmlFor="images" className="drop-container">
+                                <span className="drop-title">Drop files here</span>
+                                or
+                                <input type="file" id="images" accept="image/*" required onChange={e => setFile(e.target.files[0])} />
+                                <input hidden value={caption} onChange={e => setCaption(e.target.value)} type="text" placeholder='Caption'></input>
+                            </label>
+                        </div>
+                        <button className={"submit-btn"} type="submit">Upload</button>
+                    </div>
+
+
+                </form>
+            </div>
+        </>
     )
 }
--- front-end-react/src/pages/Profile.js
+++ front-end-react/src/pages/Profile.js
@@ -16,13 +16,23 @@ export function Profile(props) {
         //convertImageToURL(user.avatar, setUrl, props.token);
     }, [props.token, userDetail]);
 
+    const open = () => {
+        const uploadForm = document.querySelector('.upload-avatar-form');
+        const uploadContainer = document.querySelector('.upload-avatar-container');
+        if (!uploadForm.classList.contains("open")) {
+            uploadForm.classList.add("open");
+            uploadContainer.classList.add("open");
+        }
+    }
+
     return(
         <div className={'profile-container'}>
             <div className="profile-header">
 
                 <div className="cover-image"></div>
                 <div className="user_details">
-                    <div className="user-image" style={{backgroundImage: `url(https://social-site-facebook-copy-project.s3.eu-central-1.amazonaws.com/${user.avatar || "defaultAvatar.jpg"})`}} ></div>
+                    <div className="user-image" onClick={() => open()} style={{backgroundImage: `url(https://social-site-facebook-copy-project.s3.eu-central-1.amazonaws.com/${user.avatar || "defaultAvatar.jpg"})`}} ></div>
+                    <UploadAvatar userDetail={userDetail} />
                     <div className="user-content">
                         <div className="user-name">
                             <h1>{user.username}</h1>
@@ -33,7 +43,7 @@ export function Profile(props) {
                             <Link to={"/profile/friends"}><div className="tab-menu-link">Friends</div></Link>
                             <Link to={"/profile/photos"}><div className="tab-menu-link">Photos</div></Link>
                         </div>
-                        <UploadAvatar userDetail={userDetail} />
+
                     </div>
                 </div>
 
