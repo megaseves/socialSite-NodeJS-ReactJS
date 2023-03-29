efcdb197a40445f96d8e971875e15eca47cede90
megaseves <megaseves@gmail.com>
2023-03-29 20:17:06 +0200

    Add: friends setting (unfriend)

--- front-end-react/src/components/Friends/Friends.js
+++ front-end-react/src/components/Friends/Friends.js
@@ -1,13 +1,42 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useRef} from 'react';
 import './Friends.css';
 import {getAllFriend} from "../../api/ApiFetch";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faEllipsis} from "@fortawesome/free-solid-svg-icons";
 
 
 export function Friends(props) {
 
+    let ref = useRef();
+
     useEffect( () => {
         getAllFriend(props.user_id, props.token, props.setFriends);
     }, [props.setFriends, props.user_id, props.token]);
+
+    const open = (friend_id) => {
+        const setting = document.querySelector(`.setting${friend_id}`);
+        const setting_container = setting.querySelector('[data-setting-container]');
+        if (!setting_container.classList.contains("open")) {
+            setting_container.classList.add("open");
+        }
+    }
+
+    useEffect(() => {
+        let handler = (event) => {
+            const settings = document.querySelectorAll('.open');
+            if (!ref.current.contains(event.target)) {
+                settings.forEach(setting =>{
+                    setting.classList.remove('open');
+                })
+            }
+        }
+        document.addEventListener("mousedown", handler);
+
+        return () => {
+            document.removeEventListener("mousedown", handler);
+        }
+    });
+
 
     return(
         <div className="friends-container">
@@ -25,14 +54,31 @@ export function Friends(props) {
                     <div className="friend-card" key={friend.friend_id}>
                         <div className="friend-card-avatar" onClick={()=> window.location.assign(`/profile/${friend.friend_id}`)} style={{backgroundImage: `url(https://social-site-facebook-copy-project.s3.eu-central-1.amazonaws.com/${friend.avatar || "defaultAvatar.jpg"})`}} ></div>
                         <div className="friend-card-details">
-                            <span className="friend-card-name" onClick={()=> window.location.assign(`/profile/${friend.friend_id}`)}>
-                            {friend.username}
-                        </span>
-                        <span className="friend-card-email">
-                            {friend.email}
-                        </span>
+
+                            <div className="fcd-name-email">
+                                <span className="friend-card-name" onClick={()=> window.location.assign(`/profile/${friend.friend_id}`)}>
+                                    {friend.username}
+                                </span>
+                                <span className="friend-card-email">
+                                    {friend.email}
+                                </span>
+                            </div>
+                            <div className={'setting'+friend.friend_id} onClick={() => open(friend.friend_id)}>
+                                <div className="fcd-settings" >
+                                        <FontAwesomeIcon className="fcd-setting-icon" icon={faEllipsis} />
+                                        <span ref={ref} className="fcd-setting-container" data-setting-container={friend.friend_id}>
+                                            <span className="unfriend">
+                                                Unfriend
+                                            </span>
+                                        </span>
+                                </div>
+                            </div>
+
                         </div>
+
+
                     </div>
+
                 )}
             </div>
         </div>
